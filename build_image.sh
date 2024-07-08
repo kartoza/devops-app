ec73be04105dfa228bb56c80bc0f0f57d7ada4ac
LokoMoloko98 <communication@moloko-mokubedi.co.za>
2024-07-08 20:04:12 +0200

    🔧 image-index placed

--- build_image.sh
+++ build_image.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Define the image name
-IMAGE_NAME="kartoza_devops_app"
+IMAGE_NAME="kartoza_devops_app2"
 
 # Build the Docker image
 docker build -t $IMAGE_NAME .
 
-# Print a message indicating the build process is complete
-echo "Docker image '$IMAGE_NAME' built successfully."
